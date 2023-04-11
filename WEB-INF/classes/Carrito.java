5c2000a4476c87929c8542447177a6dc415733d6
Tomas G B <tomas.garcia.prof@gmail.com>
2023-04-11 18:11:57 +0200

    WEB_INF -> WEB-INF

--- WEB-INF/classes/Carrito.java
+++ WEB-INF/classes/Carrito.java
@@ -5,19 +5,21 @@ import javax.servlet.http.*;
 public class Carrito extends HttpServlet {
     private String returnPrice() {
         // Recuperamos informacion del precio del CD
+        /*
         StringTokenizer t = new StringTokenizer(descripcionCD,"|");
         t.nextToken();
         t.nextToken();
         t.nextToken();
         precioString = t.nextToken();
         precioString = precioString.replace('$',' ').trim();
+        */
+        return null;
     }
 
     public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println( "<HEAD></HEAD <BODY>\n" +
-                "<H1>Carrito</H1>\n"
-                );
+                "<H1>Carrito</H1>\n");
     }
 }
